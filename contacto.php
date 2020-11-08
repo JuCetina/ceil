ed2a1640b7b3d753eed163d74a11180f7f72cad1
JuCetina <juliethcetina@gmail.com>
2020-11-08 17:28:50 -0500

    Se mejora la funcionalidad de contacto

--- contacto.php
+++ contacto.php
@@ -11,14 +11,13 @@
 
     if(isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['email']) && isset($_POST['tel']) && isset($_POST['mensaje']))
     {
-        //var_dump($_POST);
 
-        $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
-        $apellido = filter_var($_POST['apellido'], FILTER_SANITIZE_STRING);
-        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $nombre = filter_var(trim($_POST['nombre']), FILTER_SANITIZE_STRING);
+        $apellido = filter_var(trim($_POST['apellido']), FILTER_SANITIZE_STRING);
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', trim($_POST['email']));
         $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-        $tel = filter_var($_POST['tel'], FILTER_VALIDATE_INT);
-        $mensaje = htmlspecialchars($_POST['mensaje'])." Mi número de contacto es ".$tel;
+        $tel = filter_var(trim($_POST['tel']), FILTER_VALIDATE_INT);
+        $mensaje = htmlspecialchars(trim($_POST['mensaje']))." Mi número de contacto es ".$tel;
 
         $headers  = 'MIME-Version: 1.0' . "\r\n"
         .'Content-type: text/html; charset=utf-8' . "\r\n"
